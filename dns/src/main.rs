072a0d6660478d70d358c6cbd838ba9eec20fe84
AbrahumLink <307887491@qq.com>
2021-10-13 16:15:11 +0800

    ➖ hyper replace reqwest

--- dns/src/main.rs
+++ dns/src/main.rs
@@ -1,3 +1,9 @@
+use hyper::{
+    body::{aggregate, Buf},
+    Client, Uri,
+    client::connect::HttpConnector
+};
+use hyper_tls::HttpsConnector;
 use serde::Deserialize;
 use std::{path::PathBuf, str::FromStr};
 
@@ -49,7 +55,8 @@ static GITHUBS: [&str; 35] = [
 async fn main() {
     // check DNS server quality
     let mut rstr = String::default();
-    let cli = reqwest::Client::new();
+    let https = hyper_tls::HttpsConnector::new();
+    let cli = hyper::Client::builder().build::<_, hyper::Body>(https);
     println!("Quality checking...");
     let index = quality_check(&cli).await;
     println!("Quality check done, geting host from {}", URLS[index]);
@@ -73,7 +80,8 @@ async fn main() {
     println!("{} has what you need", FILE_NAME);
 }
 
-async fn quality_check(cli: &reqwest::Client) -> usize {
+/// check DNS server quality
+async fn quality_check(cli: &Client<HttpsConnector<HttpConnector>>) -> usize {
     for i in 0..URLS.len() {
         if let Some(_) = get_host(cli, URLS[i], GITHUBS[0]).await {
             return i;
@@ -82,33 +90,42 @@ async fn quality_check(cli: &reqwest::Client) -> usize {
     panic!("All DNS server unreachable")
 }
 
-async fn get_host(cli: &reqwest::Client, dns: &'static str, url: &'static str) -> Option<Resp> {
-    let req = cli
-        .get(dns)
-        .query(&[
-            ("ct", "application/dns-json"),
-            ("name", url),
-            ("type", "A"),
-            ("do", "false"),
-            ("cd", "false"),
-        ])
-        .send();
-    if let Ok(r) = tokio::time::timeout(tokio::time::Duration::from_secs(2), req).await {
-        Some(r.unwrap().json::<Resp>().await.unwrap())
-    } else {
-        None
+/// get a url's host from dns server
+async fn get_host(
+    cli: &Client<HttpsConnector<HttpConnector>>,
+    dns: &'static str,
+    url: &'static str,
+) -> Option<Resp> {
+    let uri = format!(
+        "{}?ct=application/dns-json&name={}&type=A&do=false&cd=false",
+        dns, url
+    )
+    .parse::<Uri>()
+    .unwrap();
+    match cli.get(uri).await {
+        Ok(resp) => {
+            let bytes = aggregate(resp).await.unwrap();
+            let r = serde_json::from_reader(bytes.reader()).unwrap();
+            Some(r)
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            None
+        }
     }
 }
 
+/// format the host urlname
 fn format_host(host: &str, name: &str) -> String {
     let mut r = format!("{}", host);
-    for _ in 0..18-host.len() {
+    for _ in 0..18 - host.len() {
         r.push(' ');
     }
     r.push_str(name);
     r
 }
 
+/// resp
 #[derive(Deserialize, Debug)]
 pub struct Resp {
     #[serde(rename = "Status")]
@@ -117,6 +134,7 @@ pub struct Resp {
     pub answer: Vec<Hosts>,
 }
 
+/// resp Answer
 #[derive(Deserialize, Debug)]
 pub struct Hosts {
     pub name: String,
